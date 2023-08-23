562af10bc805d311c93a16118cc82705787ce6ca
yihcathy <huangyi1719@gmail.com>
2023-08-22 23:33:50 -0700

    change herosection css

--- trvl-website/src/components/HeroSection.js
+++ trvl-website/src/components/HeroSection.js
@@ -6,17 +6,17 @@ import '../App.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <video src="../../videos/video-2.mp4" autoPlay loop muted />
+        <video src="/videos/video-1.mp4" autoPlay loop muted />
         <h1>ADVENTURE AWAITS</h1>
         <p>What are you waiting for?</p>
         <div className="hero-btns">
-            <Button className='btn' 
+            <Button className='btns' 
                     buttonStyle='btn--outline'
                     buttonSize='btn--large'
             >
                 GET STARTED
             </Button>
-            <Button className='btn' 
+            <Button className='btns' 
                     buttonStyle='btn--primary'
                     buttonSize='btn--large'
             >
